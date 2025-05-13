3c09970cd1f714faafc727f145e66e5cff1fbffc
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2025-05-14 02:41:49 +0530

    middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import SignUpForm from "@/components/SignUpForm";
 export default function Home() {  
   return (
     <div>
       <h1>Hello World</h1>
+      <SignUpForm/>
+      
     </div>
   );
 }
